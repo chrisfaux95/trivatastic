ed9e11c4f498a7fd62746ed632610d39a08fb931
Frank Buono <fbuonojr@fordham.edu>
2020-09-01 13:28:50 -0400

    put api call in assets, deleted api test js

--- public/assets/js/quiz_assets.js
+++ public/assets/js/quiz_assets.js
@@ -53,3 +53,31 @@ types.forEach(e => {
     d.append(i,l);
     $("#types").append(d);
 });
+
+
+$(".categoryBtn").on("click", function() {
+    // var value = $(this).val();
+    // var difficulty = $(this).data...
+    //var type = ...
+    const MAXAMNT = 50;
+    quizAjax(MAXAMNT, value, difficulty, type)
+    });
+    
+    function quizAjax(amntNum, catNum, type, difficulty) {
+        $.ajax({
+            url: "https://opentdb.com/api.php?amount=" + amntNum + "&category=" + catNum + "&difficulty=" + difficulty + "&type=" + qType,
+            method: "GET"
+        }).then(function (res) {
+            if (res.response_code === 0) {
+                console.log(res);
+                count++;
+                console.log(count);
+            }
+            else {
+                if (amntNum > 0) {
+                    amntNum--;
+                    quizAjax(amntNum, i, type, difficulty);
+                }
+            }
+        });
+    }  
