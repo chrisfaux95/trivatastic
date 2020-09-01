628da805f6876e709d5cf37012ea2825b1278bcd
Dando513 <49767042+Dando513@users.noreply.github.com>
2020-08-31 21:14:57 -0400

    add user input to sql

--- server.js
+++ server.js
@@ -1,20 +1,36 @@
-var express = require("express");
+// Requiring necessary npm packages
+const express = require("express");
+const session = require("express-session");
+// Requiring passport as we've configured it
+const passport = require("./config/passport");
 
-var app = express();
-var PORT = process.env.PORT || 8080;
+// Setting up port and requiring models for syncing
+const PORT = process.env.PORT || 8080;
+const db = require("./models");
 
-var db = require("./models");
-
-app.use(express.urlencoded({ extended: true}));
+// Creating express app and configuring middleware needed for authentication
+const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-// INSERT ROUTES HERE
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
-
-db.sequelize.sync({ force: true }).then(() => {
-    app.listen(PORT, () => {
-        console.log("App listening on PORT " + PORT);
-    })
-})
+// Syncing our database and logging a message to the user upon success
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(
+      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+});
