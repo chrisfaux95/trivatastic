163dccedd09b46681b6cb80954791b3836d0f712
chrisfaux95 <chrisfaux95@gmail.com>
2020-09-02 15:20:07 -0400

    created api calls to get category by name or id

--- routes/category-api-routes.js
+++ routes/category-api-routes.js
@@ -1,8 +1,28 @@
 const db = requier("../models");
 module.exports = (app) => {
+    // API CALL TO GET LIST OF CATEGORIES
     app.get("/api/categories", (req, res) => {
         db.Category.findAll({}).then(dbCategories => {
             res.json(dbCategories);
+        });
+    });
+
+    // API CALL TO GET CATEGORY BY ID
+    app.get("/api/category_by_id/:id", (req, res) => {
+        db.Category.findOne({
+            where: { id: req.params.id }
+        }).then((dbCategory) => {
+            res.json(dbCategory);
+        })
+    })
+
+
+    // API CALL TO GET CATEGORY BY CATEGORY NAME
+    app.get("/api/category_by_name/:name", (req, res) => {
+        db.Category.findOne({
+            where: { name: req.params.name }
+        }).then((dbCategory) => {
+            res.json(dbCategory);
         })
     })
 }
