db2511d9112e3994d31f4f20ca272c1bd357b343
chrisfaux95 <chrisfaux95@gmail.com>
2020-09-02 13:27:48 -0400

    added return of username for /api/user_data

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -47,7 +47,8 @@ module.exports = function(app) {
       // Sending back a password, even a hashed password, isn't a good idea
       res.json({
         email: req.user.email,
-        id: req.user.id
+        id: req.user.id,
+        username: req.user.username
       });
     }
   });
