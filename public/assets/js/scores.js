130e95838621a3d3b5ec2efcd2dfc99141eda929
Frank Buono <fbuonojr@fordham.edu>
2020-09-03 15:16:48 -0400

    got score from db

--- public/assets/js/scores.js
+++ public/assets/js/scores.js
@@ -4,10 +4,21 @@ $(document).ready(function () {
     userData = data;
     console.log("USER DATA: ", userData);
 
-    $.get("/api/scores/by_user/" + userData.username).then(data => {
-      console.log(data); 
+    $.get("/api/scores/by_user/" + userData.username).then(function(data){
+      console.log(data);
+      var scoresContainer = $("#scoresContainer");
+      var userEl = $("<h2>").text(data[0].user.username);
+      scoresContainer.append(userEl);
+      for (var i = 0; i < data.length; i++) {
+        console.log(i);
+        var categoryEl = $("<p>").text(data[i].category.name);
+        var scoreEl = $("<p>").text(data[i].score);
+
+        scoresContainer.append(categoryEl);
+        scoresContainer.append(scoreEl);
+      }
     });
   });
 
-  
+
 })
