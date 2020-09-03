c7243fe575cd76c03045ee9509a8ffb1ba2cf833
Frank Buono <fbuonojr@fordham.edu>
2020-09-03 14:30:48 -0400

    get scores by user

--- public/assets/js/scores.js
+++ public/assets/js/scores.js
@@ -1,7 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
   var userData = "";
   $.get("/api/user_data").then(data => {
-      userData = data;
-      console.log("USER DATA: ", data);
+    userData = data;
+    console.log("USER DATA: ", data);
+  });
+
+  $.get("/api/scores/by_user/" + userData.username).then(data => {
+    console.log(data); 
   });
 })
