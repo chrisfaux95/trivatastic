7ef07cca43a47798b9c1c9d83de4f1bc5018a776
Frank Buono <fbuonojr@fordham.edu>
2020-09-03 15:27:13 -0400

    dynamically created row and col

--- public/assets/js/scores.js
+++ public/assets/js/scores.js
@@ -8,14 +8,20 @@ $(document).ready(function () {
       console.log(data);
       var scoresContainer = $("#scoresContainer");
       var userEl = $("<h2>").text(data[0].user.username);
-      scoresContainer.append(userEl);
+      $("#usernameRow").append(userEl);
       for (var i = 0; i < data.length; i++) {
+        var rowEl = $("<div>").addClass("row");
+        var colEl1 = $("<div>").addClass("col-md-6");
+        var colEl2 = $("<div>").addClass("col-md-6");
+
         console.log(i);
         var categoryEl = $("<p>").text(data[i].category.name);
         var scoreEl = $("<p>").text(data[i].score);
+        colEl1.append(categoryEl);
+        colEl2.append(scoreEl);
+        rowEl.append(colEl1, colEl2);
 
-        scoresContainer.append(categoryEl);
-        scoresContainer.append(scoreEl);
+        scoresContainer.append(rowEl);
       }
     });
   });
