931a30d364c4bb8b3cda02e147a08de6199e5630
Frank Buono <fbuonojr@fordham.edu>
2020-09-03 14:59:36 -0400

    grabbed score from db

--- public/assets/js/scores.js
+++ public/assets/js/scores.js
@@ -1,11 +1,13 @@
+var userData;
 $(document).ready(function () {
-  var userData = "";
   $.get("/api/user_data").then(data => {
     userData = data;
-    console.log("USER DATA: ", data);
+    console.log("USER DATA: ", userData);
+
+    $.get("/api/scores/by_user/" + userData.username).then(data => {
+      console.log(data); 
+    });
   });
 
-  $.get("/api/scores/by_user/" + userData.username).then(data => {
-    console.log(data); 
-  });
+  
 })
